2c865e2443097b8b379571e12ffbaf44169333be
daimingzhi <dmzitman@163.com>
2021-08-15 20:15:59 +0800

    issue version

--- skywalking-test/src/main/java/com/easy4coding/agent/MainClass.java
+++ skywalking-test/src/main/java/com/easy4coding/agent/MainClass.java
@@ -8,9 +8,26 @@ import java.lang.reflect.Method;
  */
 public class MainClass {
     public static void main(String[] args) throws Exception {
-        Class<?> aClass = Class.forName("com.easy4coding.jar.JarUtils");
-        System.out.println(aClass.getName() + "loaded by" + aClass.getClassLoader());
-        Method say = aClass.getDeclaredMethod("say");
-        say.invoke(null);
+
+        // non static interceptor test
+        Class<?> aClass1 = Class.forName("com.easy4coding.jar.Person");
+        Object o = aClass1.getConstructor(String.class).newInstance("dmz");
+        Method getName = aClass1.getMethod("getName");
+        System.out.println(getName.invoke(o));
+
+
+//        // static interceptor test
+//        Class<?> aClass2 = Class.forName("com.easy4coding.jar.JarUtils");
+//        System.out.println(aClass2.getName() + "loaded by" + aClass2.getClassLoader());
+//        Method say = aClass2.getDeclaredMethod("say");
+//        say.invoke(null);
+//
+////        System.in.read();
+//
+//        // constructor interceptor test
+//        Class<?> constructorTestClass = Class.forName("com.easy4coding.jar.Son");
+//        Object dmz = constructorTestClass.getConstructor(String.class).newInstance("dmz");
+//
+//        System.in.read();
     }
 }
